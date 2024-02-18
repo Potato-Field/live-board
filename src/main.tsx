99e984938d18a874e2c5d46b301c02f57c7637d3
piu0887 <147304229+piu0887@users.noreply.github.com>
2024-02-19 00:02:35 +0900

    충돌수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToolProvider } from './component/ToolContext';
 import { ColorProvider } from './component/ColorContext';
 import './index.css'
-import Login from './login.tsx'
+import Login from './login.tsx';
 import SignUp from './signup.tsx';
 import Lobby from './Lobby.tsx';
 import App from './App';
